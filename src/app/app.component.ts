28aeaed9a102302552d99f39916e7e6e344cac03
dmc21 <davidmoracaceres20@gmail.com>
2021-11-26 20:36:06 +0100

    todo-angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,48 @@
 import { Component } from '@angular/core';
-
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { DropEvent } from 'angular-draggable-droppable';
+import { Task } from './interfaces/Task';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
 export class AppComponent {
-  title = 'todo-angular';
+
+  className: string = "hidden-opacity"
+
+  tasks: Task[] = [
+    {name: "Task 1", done: false},
+    {name: "Task 21", done: false},
+    {name: "Task 3", done: false},
+    {name: "Task 5", done: false},
+    {name: "Task 8", done: false},
+  ]
+
+  taskForm = new FormGroup({
+    name: new FormControl("", Validators.required),
+  });
+
+  onSubmit() {
+      this.tasks.push({name: this.taskForm.value.name, done: false});
+
+      console.log(this.tasks);
+
+      this.taskForm.reset()
+  }
+
+  onDelete({ dropData }: DropEvent<Task>) {
+   const index = this.tasks.indexOf(dropData);
+   this.tasks.splice(index, 1);
+  
+  }
+
+  dragEnd(event){
+    this.className = "hidden-opacity"
+  }
+
+  dragStart(event) {
+    this.className = "show-opacity"
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
+import { DragAndDropModule } from 'angular-draggable-droppable';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    DragAndDropModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
